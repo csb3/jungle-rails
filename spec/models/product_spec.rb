8593e9077d09a5e90508efda475a76c2a2dd024d
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-09 02:46:53 +0000

    fix product rspec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,25 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'validates name' do
+    it 'saves a record successfully when all fields are valid' do
       @product = Product.create!({ :name => 'Shiny new product', :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"}) })
-      expect(@product.name).to be_truthy
     end
-    it 'validates price' do
-      @product = Product.create!({ :name => 'Shiny new product', :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"})  })
-      expect(@product.price).to be_truthy
-    end
-    it 'validates quantity' do
-      @product = Product.create!({ :name => 'Shiny new product', :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"})  })
-      expect(@product.quantity).to be_truthy
-    end
-    it 'validates category' do
-      @product = Product.create!({ :name => 'Shiny new product', :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"})  })
-      expect(@product.category_id).to be_truthy
-    end
-    it 'returns an error if a field is nil' do
-      @product = Product.create({ :name => nil, :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"})  })
+
+    it 'validates name' do
+      @product = Product.create({ :name => nil, :price_cents => 112, :quantity => 5, :category => Category.create!({ :name => "Furniture"}) })
       expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'validates price' do
+      @product = Product.create({ :name => 'Shiny new product', :price_cents => nil, :quantity => 5, :category => Category.create!({ :name => "Furniture"})  })
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'validates quantity' do
+      @product = Product.create({ :name => 'Shiny new product', :price_cents => 112, :quantity => nil, :category => Category.create!({ :name => "Furniture"})  })
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'validates category' do
+      @product = Product.create({ :name => 'Shiny new product', :price_cents => 112, :quantity => 5, :category => nil  })
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
